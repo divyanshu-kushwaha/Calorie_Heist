aa023fc67fac3b231601b235b3f0c48e29222042
Amrit Raj <amritraj430@gmail.com>
2022-03-13 04:25:01 -0700

    bmi and calorie info added

--- app.js
+++ app.js
@@ -134,6 +134,51 @@ app.get("/profile/:currentUser", (req,res)=>{
     })
 })
 
+var calorie=0;
+var bmi=0;
+var nutrient=0;
+
+app.get("/bmi", function(req, res) {
+  res.render("bmi" , {cal:calorie , bmi:bmi, nutrient:nutrient});
+});
+
+app.post("/bmi" , function(req,res){
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+    var weight1 = parseFloat(req.body.weight1);
+    var height1 = parseFloat(req.body.height1);
+    var age = parseFloat(req.body.age);
+    var gender = req.body.sex;
+    var k = parseFloat(req.body.activity);
+    var result
+    var result1 = weight1 / (height1/100 * height1/100);
+    if(gender == "male"){
+      result = 10*weight + 6.25*height - 5*k +5
+    }
+
+    else result = 10*weight + 6.25*height - 5*k - 161
+    calorie = Math.floor(result)
+    bmi = result1;
+
+    //API part
+    var food = req.body.food;
+    const url = "https://api.spoonacular.com/recipes/complexSearch?query=" + food + "&apiKey=ca35cc0fdf2347c8b85bf8e05d140518&number=1&addRecipeNutrition=true";
+    https.get(url, function (response) {
+      console.log(response.statusCode);
+      var chunks = [];
+        response.on("data", function (chunk) {
+          chunks.push(chunk);
+        });
+        response.on("end", function (chunk) {
+          var body = Buffer.concat(chunks);
+          var data = JSON.parse(body);
+          var result2 = Math.floor(data.results[0].nutrition.nutrients[0].amount);
+          nutrient = result2
+    });
+    })
+    res.redirect("/bmi");
+});
+
 app.get("/logout", (req,res)=>{
   req.logout();
   res.redirect("/");
@@ -177,6 +222,53 @@ app.post("/login", (req,res)=>{
     }
   })
 })
+
+var calorie=0;
+var bmi=0;
+var nutrient=0;
+
+app.get("/bmi", function(req, res) {
+  res.render("bmi" , {cal:calorie , bmi:bmi, nutrient:nutrient});
+});
+
+app.post("/bmi" , function(req,res){
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+    var weight1 = parseFloat(req.body.weight1);
+    var height1 = parseFloat(req.body.height1);
+    var age = parseFloat(req.body.age);
+    var gender = req.body.sex;
+    var k = parseFloat(req.body.activity);
+    var result
+    var result1 = weight1 / (height1/100 * height1/100);
+    if(gender == "male"){
+      result = 10*weight + 6.25*height - 5*k +5
+    }
+
+    else result = 10*weight + 6.25*height - 5*k - 161
+    calorie = Math.floor(result)
+    bmi = result1;
+
+    //API part
+    var food = req.body.food;
+    var API_KEY = process.env.API_KEY;
+    const url = "https://api.spoonacular.com/recipes/complexSearch?query=" + food + "&apiKey=" + API_KEY + "&number=1&addRecipeNutrition=true";
+    https.get(url, function (response) {
+      console.log(response.statusCode);
+      var chunks = [];
+        response.on("data", function (chunk) {
+          chunks.push(chunk);
+        });
+        response.on("end", function (chunk) {
+          var body = Buffer.concat(chunks);
+          var data = JSON.parse(body);
+          var result2 = Math.floor(data.results[0].nutrition.nutrients[0].amount);
+          nutrient = result2
+    });
+    })
+    res.redirect("/bmi");
+});
+
 
 /////////////////////////////PORT////////////////////////////////////
 
