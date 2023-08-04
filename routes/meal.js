e2e7d8903660b5ac183e2f5381c1e1e5b587f1cb
Divyanshu Kushwaha <divyanshukushwaha654@gmail.com>
2023-08-04 19:11:13 +0530

    displays nutrients' table

--- routes/meal.js
+++ routes/meal.js
@@ -1,53 +1,39 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const router = express.Router();
+const request = require("request");
 const https = require("https");
+const passport = require("passport");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 
-var nutrients = 0;
-const dietSchema = new mongoose.Schema({
-    date: String,
-    calorie: Number,
-}); 
-router.get("/", function(req, res) {
-    res.render("meal", {
-        nutrients: nutrients
-    });
+router.get("/", function (req, res){
+    res.render('meal', { foodItemList: null }); // Initialize jsonData with null
 });
 
-router.post("/", function(req, res) {
-    var currUserObject = req.user;
-
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, "0");
-    var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-    var yyyy = today.getFullYear();
-    today = mm + "/" + dd + "/" + yyyy;
-    //API part
-    var food = req.body.food;
-    var API_KEY = process.env.API_KEY;
-    const url1 =
-        "https://api.spoonacular.com/recipes/complexSearch?query=" +
-        food +
-        "&apiKey=" +
-        API_KEY +
-        "&number=1&addRecipeNutrition=true";
-
-    //HTTPS get
-    https.get(url1, function(response) {
-        console.log(response.statusCode);
-        var chunks = [];
-        response.on("data", function(chunk) {
-            chunks.push(chunk);
-        });
-        response.on("end", function(chunk) {
-            var body = Buffer.concat(chunks);
-            var data = JSON.parse(body);
-            var result2 = Math.floor(data.results[0].nutrition.nutrients[0].amount);
-            nutrients = result2;
-        });
-    });
-
-    res.redirect("/meal");
+router.post("/", function (req, res) {
+    const food = req.body.food;
+    const API_KEY = process.env.API_KEY;
+    request.get(
+        {
+            url: "https://api.calorieninjas.com/v1/nutrition?query=" + food,
+            headers: {
+                "X-Api-Key": API_KEY,
+            },
+        },
+        function (error, response, body) {
+            if (error) return console.error("Request failed:", error);
+            else if (response.statusCode !== 200)
+                return console.error(
+                    "Error:",
+                    response.statusCode,
+                    body.toString("utf8")
+                );
+                else {
+                    const foodItemList = JSON.parse(body).items; // Parse the JSON response
+                    res.render('meal', { foodItemList }); // Render meal.ejs with jsonData
+                }
+        }
+    );
 });
+
 module.exports = router;
