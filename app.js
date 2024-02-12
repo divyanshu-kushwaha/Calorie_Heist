c062e10d70c708126fefa335594cbfcae04db51b
Divyanshu Kushwaha <divyanshukushwaha654@gmail.com>
2024-02-13 04:19:25 +0530

    minor refactor

--- app.js
+++ app.js
@@ -9,15 +9,20 @@ const ejs = require("ejs");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-const bmiRoute = require('./routes/bmi');
-const mealRoute = require('./routes/meal');
+const bmiRoute = require("./routes/bmi");
+const mealRoute = require("./routes/meal");
+
+const userSchema = require("./models/userSchema");
+const dietSchema = require("./models/dietSchema");
 
 const app = express();
 
 //ejs environment
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
@@ -42,33 +47,12 @@ mongoose.connect(process.env.DATABASE, {
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function() {
+db.once("open", function () {
     console.log("Connected to Database successfully");
 });
 
-////////////////////////////// SCHEMA /////////////////////////////////////
-
-const userSchema = new mongoose.Schema({
-    first: String,
-    last: String,
-    username: String,
-    password: String,
-    bmi: Number,
-    cal: Number,
-    weight: Number,
-    height: Number,
-    gender: String
-});
-
-const dietSchema = new mongoose.Schema({
-    date: String,
-    calorie: Number,
-});
-
-userSchema.plugin(passportLocalMongoose);
-
 ///////////////////////// PASSPORT //////////////////////////////////////////
-
+userSchema.plugin(passportLocalMongoose);
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
@@ -77,18 +61,18 @@ passport.deserializeUser(User.deserializeUser());
 
 ////////////////////////////// ROUTES /////////////////////////////////
 
-app.use('/bmi', bmiRoute.router);
-app.use('/meal', mealRoute);
+app.use("/bmi", bmiRoute.router);
+app.use("/meal", mealRoute);
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.render("home");
 });
 
-app.get("/register", function(req, res) {
+app.get("/register", function (req, res) {
     res.render("register");
 });
 
-app.get("/login", function(req, res) {
+app.get("/login", function (req, res) {
     res.render("login");
 });
 
@@ -97,7 +81,7 @@ app.get("/profile/:currentUser", (req, res) => {
     // console.log(req.user);
     const currUserObject = req.user;
     res.render("dashboard", {
-        currentUserObj: currUserObject
+        currentUserObj: currUserObject,
     });
 });
 
@@ -123,7 +107,7 @@ app.post("/register", (req, res) => {
             console.log(err);
             res.redirect("/register");
         } else {
-            passport.authenticate("local")(req, res, function() {
+            passport.authenticate("local")(req, res, function () {
                 res.redirect("/update");
             });
         }
@@ -135,42 +119,43 @@ app.post("/login", (req, res) => {
         username: req.body.username,
         password: req.body.password,
     });
-    req.login(user, function(err) {
+    req.login(user, function (err) {
         if (err) {
             console.log(err);
         } else {
-            passport.authenticate("local")(req, res, function() {
+            passport.authenticate("local")(req, res, function () {
                 res.redirect("/profile/" + req.user.username);
             });
         }
     });
 });
 
-
 //BMI
 
-
-app.get("/update", function(req, res) {
+app.get("/update", function (req, res) {
     if (req.isAuthenticated()) {
-        User.find({
-            secret: {
-                $ne: null
-            }
-        }, function(err, foundUser) {
-            if (err) {
-                console.log(err);
-            } else {
-                if (foundUser) {
-                    res.render("update");
+        User.find(
+            {
+                secret: {
+                    $ne: null,
+                },
+            },
+            function (err, foundUser) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    if (foundUser) {
+                        res.render("update");
+                    }
                 }
             }
-        });
+        );
     } else {
         res.redirect("/register");
     }
 });
 
-app.post("/update", function(req, res) {
+app.post("/update", function (req, res) {
     var currUserObj = req.user;
     var weight = parseFloat(req.body.weight);
     var height = parseFloat(req.body.height);
@@ -183,34 +168,29 @@ app.post("/update", function(req, res) {
     var ans = Math.floor(bmr);
     var bmi = bmiRoute.calculateBMI(weight, height);
 
-    User.findById(currUserObj._id, function(err, foundUser){
-      if(err){
-        console.log(err);
-      }
-      else{
-        if(foundUser){
-          {
-              foundUser.weight = weight,
-              foundUser.height = height,
-              foundUser.bmi = bmi,
-              foundUser.cal = ans,
-              foundUser.gender = gender
-          };
+    User.findById(currUserObj._id, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                {
+                    (foundUser.weight = weight),
+                        (foundUser.height = height),
+                        (foundUser.bmi = bmi),
+                        (foundUser.cal = ans),
+                        (foundUser.gender = gender);
+                }
 
-          foundUser.save(function(){
-            res.redirect("/profile/" + currUserObj.username);
-          })
+                foundUser.save(function () {
+                    res.redirect("/profile/" + currUserObj.username);
+                });
+            }
         }
-      }
-    })
-
+    });
 });
-
-
-
 
 /////////////////////////////PORT////////////////////////////////////
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server is running on port 3000.");
 });
